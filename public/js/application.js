3bd233b9e768c5753bff75ebdb6349853490edb3
Shawn Watson <shawnw86@gmail.com>
2015-11-22 12:38:11 -0800

    Serious refactor of handlers, combined all form handlers into a single function

--- public/js/application.js
+++ public/js/application.js
@@ -1,45 +1,17 @@
 $(document).ready(function() {
-	showNewUserForm();
-	showEditUserForm();
+	formHandler();
 	logoutHandler();
-	showLoginForm();
 });
 
-var showNewUserForm = function() {
-	$('body').on('click', '#show_new_user_form', function(event) { 
+var formHandler = function() { 
+	$('body').on('click', '.popup_form', function(event) {
 		event.preventDefault();
+		var targetUrl = $(this).attr('href')
 		var response = $.ajax({
 			method: 'GET',
-			url: '/users/new'
+			url: targetUrl
 		});
-		response.done(function(data) { 
-			$('.show_form').append(data);
-		});
-	});
-}
-
-var showEditUserForm = function() { 
-	$('body').on('click', '#show_edit_user_form', function(event) { 
-		event.preventDefault();
-		var userId = $(this).attr('href').match(/\d+/) 
-		var response = $.ajax({
-			method: 'GET',
-			url: '/users/' + userId + '/edit'
-		});
-		response.done(function(data){ 
-			$('.show_form').append(data);
-		});
-	});
-}
-
-var showLoginForm = function() { 
-	$('body').on('click', '#show_log_in_form', function(event) { 
-		event.preventDefault(); 
-		var response = $.ajax({
-			method: 'GET',
-			url: '/sessions/new'
-		});
-		response.done(function(data){
+		response.done(function(data) {
 			$('.show_form').append(data);
 		});
 	});
